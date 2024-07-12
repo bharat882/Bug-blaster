04b901ad93b19d694fe2bc4ea2cf80019f328deb
bharat882 <bharatsinghsaini00@gmail.com>
2024-07-12 11:37:44 -0400

    UI added for Ticketform

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import TicketForm from "./components/TicketForm";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+        <TicketForm></TicketForm>
+      </div>
+    </div>
+  );
+}
+
+export default App;
